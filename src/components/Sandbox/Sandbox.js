37ccfaccb3b91646860f37bf12cb34df55a5da5f
Shrithan <51796064+ShriD5@users.noreply.github.com>
2024-02-10 00:14:21 +0530

    Remove xterm-addon & add integrate backend api

--- src/components/Sandbox/Sandbox.js
+++ src/components/Sandbox/Sandbox.js
@@ -1,13 +1,11 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Editor from '@monaco-editor/react';
 import { Terminal } from 'xterm';
-import { FitAddon } from 'xterm-addon-fit';
 import 'xterm/css/xterm.css';
 import { Box, Flex, Button, VStack, useToast, ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 import * as sandboxService from "../../services/api";
-import {executeCode} from "../../services/api"
-
+import { executeCode } from "../../services/api";
 
 const theme = extendTheme({
     styles: {
@@ -22,7 +20,6 @@ const theme = extendTheme({
 
 const Sandbox = () => {
     let { sandboxId } = useParams();
-    console.log('Sandbox ID:', sandboxId);
 
     const [sandbox, setSandbox] = useState(null);
     const [loading, setLoading] = useState(true);
@@ -30,34 +27,27 @@ const Sandbox = () => {
     const editorRef = useRef(null);
     const terminalRef = useRef(null);
     const terminal = useRef(null);
-    const fitAddon = useRef(new FitAddon());
     const toast = useToast();
     const [output, setOutput] = useState('');
     const [codeSaved, setCodeSaved] = useState(false);
-
-
 
     useEffect(() => {
         if (terminalRef.current && !terminal.current) {
             terminal.current = new Terminal({
                 cursorBlink: true,
             });
-            terminal.current.loadAddon(fitAddon.current);
             terminal.current.open(terminalRef.current);
-            setTimeout(() => fitAddon.current.fit(), 0); // Ensure fit is called after the terminal is displayed
         }
     }, [terminalRef.current]);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                console.log({ sandboxId })
                 const data = await sandboxService.getSandboxById(sandboxId);
-                console.log({ data })
                 setSandbox(data);
                 setLoading(false);
             } catch (error) {
-                setError(error.message); // Update this line
+                setError(error.toString());
                 setLoading(false);
             }
         };
@@ -67,7 +57,6 @@ const Sandbox = () => {
 
     useEffect(() => {
         if (sandbox && editorRef.current) {
-            // Only set the default value if sandbox is not null
             editorRef.current.setValue(sandbox.code);
         }
     }, [sandbox, editorRef.current]);
@@ -75,9 +64,7 @@ const Sandbox = () => {
     const runCode = async () => {
         try {
             const code = editorRef.current.getValue();
-            console.log({code})
             const result = await executeCode(code);
-            console.log({result})
             setOutput(result);
             if (terminal.current) {
                 terminal.current.writeln(`Output:\n${result.result}`);
@@ -99,7 +86,6 @@ const Sandbox = () => {
         try {
             const code = editorRef.current.getValue();
             const updatedSandbox = await sandboxService.updateSandbox(sandboxId, code, output);
-            console.log('Sandbox updated:', updatedSandbox);
             toast({
                 title: 'Code saved.',
                 description: 'Your code has been successfully saved.',
@@ -120,6 +106,7 @@ const Sandbox = () => {
             });
         }
     };
+
     return (
         <ChakraProvider theme={theme}>
             <Flex height="100vh" overflow="hidden" direction="column" p="2">
@@ -151,4 +138,5 @@ const Sandbox = () => {
         </ChakraProvider>
     );
 };
+
 export default Sandbox;
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://noderunner.onrender.com/';
+const API_URL = 'https://noderunner.onrender.com';
 
 const signup = async (email, password) => {
     try {
